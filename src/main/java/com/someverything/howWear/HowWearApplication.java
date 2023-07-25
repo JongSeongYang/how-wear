e334af9c335b083c94547726547888d7f3be7487
JongSeong Yang <yangjs0523@naver.com>
2023-07-25 22:38:23 +0900

    Initial commit

---
+++ src/main/java/com/someverything/howWear/HowWearApplication.java
@@ -0,0 +1,13 @@
+package com.someverything.howWear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowWearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowWearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/someverything/howWear/HowWearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.someverything.howWear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowWearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
